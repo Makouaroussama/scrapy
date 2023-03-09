e9e83aebace95b90cc87f2fad7c2b95607f29dd4
RedaMakouar <makouarreda@gmail.com>
2023-03-10 00:48:49 +0100

    removed needless code from currentuser route as it wastransfered to its own middleware in case other parts of our api needs it

--- app/routes/auth/current_user.py
+++ app/routes/auth/current_user.py
@@ -1,26 +1,8 @@
 from app import auth
 from flask import request, make_response
-from jwt import PyJWT
-import os
 
 @auth.route('/currentuser')
 def currentuser():
-    authToken = request.cookies.get("auth_token")
-    res = make_response()
-    res.data = {
-        "currentuser": None
-    }
-
-    if(authToken == None):
-        return res;
-    
-    try:
-        payload = PyJWT.decode(authToken, os.getenv("JWT_KEY"))
-        res.data = {
-            "currentuser": payload
-        }
-    except:
-        pass;
-
+    res = make_response(request.environ["currentuser"])
     return res
 
